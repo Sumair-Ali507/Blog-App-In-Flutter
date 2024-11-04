76ee977332815f7db2f67db0ab8fd91cd51ef1e1
Sumair Ali <sumairaliphulpoto1232@gmail.com>
2024-11-04 12:10:34 +0500

    Blog App

---
+++ blog_app/lib/Core/Theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:blog_app/Core/Theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor
+  );
+ // static final lightThemeMode = ThemeData.light();
+
+}

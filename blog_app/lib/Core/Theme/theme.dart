c72c2cc3fc365dddd03c4803c0ca4b9162cc991a
Sumair Ali <sumairaliphulpoto1232@gmail.com>
2024-11-05 14:49:17 +0500

    Blog App

--- blog_app/lib/Core/Theme/theme.dart
+++ blog_app/lib/Core/Theme/theme.dart
@@ -1,10 +1,14 @@
 import 'package:blog_app/Core/Theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
-class AppTheme{
+class AppTheme {
   static final darkThemeMode = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: AppPallete.backgroundColor
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: const InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: AppPallete.borderColor, width: 3),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+    ),
   );
- // static final lightThemeMode = ThemeData.light();
-
 }
--- blog_app/lib/Features/Presentation/Pages/Signup.dart
+++ blog_app/lib/Features/Presentation/Pages/Signup.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/Features/Presentation/Widgets/auth_field.dart';
 import 'package:flutter/material.dart';
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -13,11 +14,20 @@ class _SignupPageState extends State<SignupPage> {
 
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
           Text('Sign Up',style: TextStyle(
-            fontSize: 30,
+            fontSize: 50,
             fontWeight: FontWeight.bold,
           ),),
+          SizedBox(height: 20,),
+          AuthField(hintText: 'Name'),
+          SizedBox(height: 20,),
+          AuthField(hintText: 'Email'),
+          SizedBox(height: 20,),
+          AuthField(hintText: 'Password'),
+          SizedBox(height: 20,),
+
 
         ],
       ),
